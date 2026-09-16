cd29d31984c296f2d918759eb50b476804cb12a5
sakurai-ai <japanesegirl228006@gmail.com>
2026-09-16 14:08:56 +0700

    ✨ Added auto-installing start.sh script

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+if [ ! -d "venv" ]; then
+    echo "🌸 Создаю виртуальное окружение (первый запуск)..."
+    python3 -m venv venv
+    venv/bin/pip install -r requirements.txt
+fi
+
+source venv/bin/activate
+
+# Запускаем Yummy-DL
+python3 main.py
